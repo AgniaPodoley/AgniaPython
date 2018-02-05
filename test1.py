12e67af6fde4f2ada0a242b0b11de4fca3c67064
lyubomyr83 <lyubomyr83@mail.ru>
2018-02-05 18:29:07 +0300

    if, elif, else

---
+++ test1.py
@@ -0,0 +1,13 @@
+print ('Ваше имя?')
+name = input ()
+print('Ваш возраст?')
+age = int(input())
+if age>=18 and age<=60:
+    myfile = open('users.txt', 'w')
+    print('Имя: ', name, ', возраст: ', age, file=myfile)
+    myfile.close()
+    print('Спасибо!')
+elif age<18:
+    print('Вы слишком молоды')
+elif age>60:
+    print('Пока')
